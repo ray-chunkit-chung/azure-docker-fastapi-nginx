21fb4589c118b9e6a8bfa5e93686e2a0b9d75339
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2023-03-08 16:14:11 +0900

    change app name. add name = __main__

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+import uvicorn
+import os
 
-my_fastapi_app = FastAPI()
+# heroku only
+PORT = os.environ.get('PORT', 8000)
+
+app = FastAPI()
 
 
-@my_fastapi_app.get("/")
+@app.get("/")
 async def root():
-    return {"Nginx": "I'm alive over TvT v9000 T.T nakimasu"}
+    return {"Nginx": "I'm alive over TvT v9000 T.T nakimasu yametekudastop!!!"}
+
+
+# heroku only
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
